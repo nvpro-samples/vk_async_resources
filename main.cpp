08cd88f0868e17177af10b15c1c6fbdfaab1e9e5
Martin-Karl Lefrançois <mlefrancois@nvidia.com>
2025-01-28 11:53:56 +0100

    Bulk Change [2025-01-28]
    Script sync from SHA: c6476480949ffcb5f787b01fbcfda4b8264f25c8

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019-2021, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2019-2025, NVIDIA CORPORATION.  All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * SPDX-FileCopyrightText: Copyright (c) 2019-2021 NVIDIA CORPORATION
+ * SPDX-FileCopyrightText: Copyright (c) 2019-2025 NVIDIA CORPORATION
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -496,12 +496,7 @@ public:
 
   void reloadShaders()
   {
-    VkResult result = vkQueueWaitIdle(m_queue);
-    if(nvvk::checkResult(result))
-    {
-      exit(-1);
-    }
-
+    NVVK_CHECK(vkQueueWaitIdle(m_queue));
     m_shaderManager.reloadShaderModules();
     initTestPipeline();
   }
